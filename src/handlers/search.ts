77b8bb77e812e52883ddd9b906d4e24f3149b2fa
ktubot <121492934+ktubot@users.noreply.github.com>
2023-11-15 14:11:55 +0530

    Update search.ts

--- src/handlers/search.ts
+++ src/handlers/search.ts
@@ -4,13 +4,13 @@ async function search(ctx: CustomContext) {
   const replyMsg = `
 Please use inline query to search for a KTU notification.
 
-Type <code>@ktu_results_bot query</code> in the bot chat and then choose the notification you want.
+Type <code>@notesktubot query</code> in the bot chat and then choose the notification you want.
 
 <b>Examples:</b>
 
-<code>@ktu_results_bot timetable</code> : To search for timetable notifications
+<code>@notesktubot timetable</code> : To search for timetable notifications
 
-<code>@ktu_results_bot calendar</code> : To search for academic calendar notifications
+<code>@notesktubot calendar</code> : To search for academic calendar notifications
 
 <i>Copy the above examples and paste it in the bot chat to see the results.</i>
 `;
