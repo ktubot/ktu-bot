4a9bdd498f6092a2e284444f775009386bd7e924
ktubot <121492934+ktubot@users.noreply.github.com>
2023-11-16 12:36:59 +0530

    Update notifications.ts

--- src/handlers/notifications.ts
+++ src/handlers/notifications.ts
@@ -3,8 +3,5 @@ import { CustomContext } from "../types/customContext.type";
 async function announcement(ctx: CustomContext) {
   await ctx.scene.enter("announcement-wizard");
 }
-  const replyMsg = `
-KTUNOTES.IN 🌐
 
-`;
 export default announcement;
